6aac26602703f2e4c3e5a658d5cbbbbe255de381
Anne Rogers <amr@cs.uchicago.edu>
2017-02-20 22:29:39 -0600

    random sample dictionary

---
+++ dict.py
@@ -0,0 +1,40 @@
+import nltk
+import random
+from nltk.corpus import gutenberg
+from nltk.corpus import sentiwordnet as swn
+
+bible = gutenberg.words('bible-kjv.txt')
+bible = [word for word in bible if word.isalpha()]
+length = len(bible)
+
+category = {'JJ': 'a', 'JJR': 'a', 'JJS': 'a', 'PRP': 'n', 'NN': 'n', 'NNS': 'n', 'NNP': 'n',
+'NNPS': 'n', 'RB': 'r', 'RBR': 'r', 'RBS': 'r', 'VBD': 'v', 'VBG': 'v', 'VBN': 'v', 'VBP': 'v', 'VBZ': 'v'}
+
+def random_dict():
+    genres_code = {1: 'service', 2: 'food', 3: 'environment', 4: 'price'}
+    sentences = []
+    word_to_score = []
+    i = 0
+    while i < 100:
+        genres = {'service': [], 'food': [], 'environment': [], 'price': []}
+        score = {}
+        start = random.randint(0, length)
+        end = start + random.randint(0, min(length - start, 40))
+        sentence = bible[start:end]
+        for word in sentence:
+            tag = nltk.pos_tag([word])
+            if tag[0][1] in category:
+                cat = category[tag[0][1]]
+                related = list(swn.senti_synsets(word,cat))
+                if related != []:
+                    genre = genres_code[random.randint(1, 4)]
+                    genres[genre].append(word)
+                    pos = related[0].pos_score()
+                    neg = related[0].neg_score()
+                    obj = related[0].obj_score()
+                    score[word] = {'pos': pos, 'neg': neg, "obj": obj}
+        sentences.append(genres)
+        word_to_score.append(score)
+        i += 1
+
+    return sentences, word_to_score
