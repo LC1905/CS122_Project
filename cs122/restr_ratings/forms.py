57f00cf635663ad4c3c429a536a57f162e52f87b
Shucen Liu <shucen@cs.uchicago.edu>
2017-03-05 21:41:08 -0600

    updated database

--- cs122/restr_ratings/forms.py
+++ cs122/restr_ratings/forms.py
@@ -2,3 +2,4 @@ from django import forms
 
 class NameForm(forms.Form):
 	restaurant = forms.CharField(label = 'Restaurant', max_length = 100)
+	neighborhood = forms.CharField(label = 'neighborhood', max_length = 100)
