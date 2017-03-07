96b81e6b1a9e39d4bec9d6d0286b60811ffaf408
Anne Rogers <amr@cs.uchicago.edu>
2017-03-07 00:13:50 -0600

    some wokr

--- cs122/restr_ratings/views.py
+++ cs122/restr_ratings/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from .forms import SearchForm
+import output
 
 def get_name(request):
-	context = {}
-	if request.method == 'POST':
-		form = SearchForm(request.POST)
-		# plot
-		return HttpResponse('thanks.')
-	else:
-		form = SearchForm()
-	context['form'] = form
-	return render(request, 'name.html', context)
+    context = {}
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            args = {}
+            restr = form.clean_data['restr']
+            if restr:
+                arg['restr'] = restr
+            nbh = form.clean_data['nbh']
+            catg = form.clean_data['catg']
+            price = form.clean_data['price']
+            if nbh and catg and price:
+                ls = sorted([[nbh,'nbh'],[price,'price'],[catg,'category']])
+                arg['order'] = [i[1] for i in ls]
+            # <<<This is the output>>>
+            # try:
+            #     restr_table = output.find_restr(args)
+            #     restr_plot = output.plot_scatter(restr_table)
+    return HttpResponse('thanks.')
+    else:
+        form = SearchForm()
+    context['form'] = form
+
+    return render(request, 'name.html', context)
